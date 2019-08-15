afcb506f1ad78b385cea24488c0272a62894ada2
faalbers <chalbers@gmail.com>
2019-08-14 19:57:44 -0700

    First commit

---
+++ QuickenStockPrices.py
@@ -0,0 +1,26 @@
+from qifparse.parser import QifParser
+from alpha_vantage.timeseries import TimeSeries
+import time
+
+ts = TimeSeries(key='5I186NCN3TF27H5P', output_format='csv')
+file = open('QuickenExport.QIF')
+qif = QifParser.parse(file)
+
+quotes = set()
+for price in qif.get_prices():
+    quotes.add(price.name)
+
+newquotes = {}
+outstring = ""
+for quote in quotes:
+    data, meta_data = ts.get_quote_endpoint(quote)
+    data = list(data)
+    if len(data) == 2:
+        price = float(list(data)[1][4])
+        print(quote)
+        outstring += "%s, %.3f\n" % (quote, price)
+    # unpayed acces to apha vantage needs 12 seconds between every parse
+    time.sleep(12)
+outfile = open('LatestStockPrices.csv', 'w')
+outfile.write(outstring)
+file.close()
